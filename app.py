574126916b01894821c5dec32525d99398574478
Amalya <amalyav7@gmail.com>
2026-08-12 17:22:24 -0400

    changing greens

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.markdown(
         --page-yellow: #FDFAE8;
         --page-yellow-soft: #FDFAE8;
         --outer-card: #FFF8AD;
-        --section-fill: #E6E9CC;
+        --section-fill: #D1F59F;
         --section-fill-2: #E9ECD3;
         --input-fill: #E7EBCF;
         --olive: #566B30;
